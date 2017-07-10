0a2e89f631d15d0448d1edd041c15d14055c7a40
pdhoward <patrick.howard@hotmail.com>
2017-07-10 15:54:42 -0400

    update

--- server.js
+++ server.js
@@ -1,106 +1,30 @@
 
 ////////////////////////////////////////////////////
-////////  			HTTP SERVER     		        ///////
+////////  			Sockets and HTTP SERVER      ///////
 ///////            version 0.5.0            ///////
 //////////////////////////////////////////////////
 
-var express     = require('express');
-var http        = require('http');
-var path        = require('path');
-var testArray   = require('./texts');
-var redis       = require('socket.io-redis');
-var socketIo    = require('socket.io');
-var uuid        = require('uuid/v4');
+'use strict';
 
-var app = express();
-var httpServer = new http.Server(app);
-var port = 3200;
-var htmlFile = path.resolve(__dirname, './client/index.html');
-var buildFolder = path.resolve(__dirname, './build');
+var express 	   = require('express');
+var app  		     = express();
+var path 		     = require('path');
+//  application components
+var ioServer 	= require('./app/socket')(app);
+
+// Set the port number
+var port = process.env.PORT || 3002;
+
+// Middlewares
+var htmlFile =      path.resolve(__dirname, './client/index.html');
+var buildFolder =   path.resolve(__dirname, './build');
+
+// messages
+var testArray   = require('./texts');
+
+app.use(session);
+
+ioServer.listen(port);
+
 
 var io = socketIo(httpServer);
-
-var room = 'board-chaotictests';
-
-var data = {
-  id: "",
-  content: "this is a test text",
-  user: "BanterBot",
-  postType: "Live",
-  likes:[],
-  dislikes:[],
-  sessionId: "testid"
-}
-
-var session = {
-  id: "board-chaotictests"
-}
-
-
-/////////////////////////////////////////////////////////////////
-////////        Cross Server Messaging Integration        ///////
-/////////////////////////////////////////////////////////////////
-
-io.adapter(redis({host: 'localhost', port: 6379}));
-
-function prepareText() {
-  var textObject = testArray[Math.floor(Math.random() * testArray.length)];
-  data.content = textObject.text;
-  data.user = textObject.name;
-
-}
-
-function sendPost(session, data, socket) {
-      socket.sessionId = session.id;
-      data.sessionId = session.id;
-      var postId = uuid();
-      data.id = postId;
-      data.postType = 'Live';
-      socket.broadcast
-      .to(room)
-      .emit('chaotic/posts/receive/add', data)
-
-      data.postType = 'AI';
-      socket.broadcast
-      .to(room)
-      .emit('chaotic/posts/receive/add', data)
-
-  }
-
-function joinSession(session, socket) {
-        console.log("JOINING SESSION")
-        socket.join(room, function() {
-        socket.sessionId = session.id;
-        });
-      };
-
-io.on('connection', function(socket) {
-
-      joinSession(session, socket);
-
-      ////////////////////////////////////////////////////////////////////////////
-      // a couple of tests to learn sockets
-      console.log(io.sockets.name) // '/'
-      console.log(io.of('/projects/test_cases').name) // '/projects/test_cases'
-      io.emit('time', new Date);
-      //var io = require('socket.io-emitter')({ host: '127.0.0.1', port: 6379 })
-      /////////////////////////////////////////////////////////////////////////////
-
-      setInterval(function(){
-        prepareText();
-        sendPost(session, data, socket);
-      }, 15000);
-
-
-    });
-
-
-
-app.use('/build', express.static(buildFolder));
-app.get('/*', function(req, res) {
-		res.sendFile(htmlFile)
-		});
-
-httpServer.listen(port);
-
-console.log("running on port " + port);
