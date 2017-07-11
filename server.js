b1956b35c31c8683a8df2026744f1e5c361f1537
pdhoward <patrick.howard@hotmail.com>
2017-07-11 17:28:58 -0400

    updates

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 
 ////////////////////////////////////////////////////
-////////  			Sockets and HTTP SERVER      ///////
+////////  			RedisLab and HTTP SERVER     ///////
 ///////            version 0.5.0            ///////
 //////////////////////////////////////////////////
 
 'use strict';
+var config 	=       require('./config');
+var express =       require('express');
+var path  =         require('path');
 
-var express 	   = require('express');
-var app  		     = express();
-var path 		     = require('path');
-//  application components
-var ioServer 	= require('./app/socket')(app);
-
-// Set the port number
+var app =  express();
+var server =        require('http').Server(app);
 var port = process.env.PORT || 3002;
 
-// Middlewares
-var htmlFile =      path.resolve(__dirname, './client/index.html');
-var buildFolder =   path.resolve(__dirname, './build');
+////////////////////////////////////////////////
+let redisPort =     config.redis.port;
+let redisHost =     config.redis.host;
+let redisPassword = config.redis.password;
 
-app.use('/', function(req, res, next) {
-  res.sendFile(htmlFile)
+var Redis =         require('ioredis');
+
+var redisx = new Redis({
+ port: redisPort,
+ host: redisHost
+});
+
+var pub = new Redis({
+ port: portx,
+ host: hostx
+
+});
+
+// subscribe and listen
+redis.subscribe('newMessage', function (err, count) {
+			console.log("Subscribed to " + count + " channel")
+  });
+
+redis.on('message', function (channel, message) {
+    console.log('Received the following from channel ' + channel + ' message: ' + message);
+  });
+
+// keep server running
+
+server.listen(port, function() {
+  console.log("Listening on port " + port)
 })
-
-ioServer.listen(port);
