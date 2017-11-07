f6dc5285ab8da8fb986b8080687dad4de0771163
pdhoward <patrick.howard@hotmail.com>
2017-11-07 12:01:33 -0500

    updates

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var config 	=       require('./config');
 var express =       require('express');
 var path  =         require('path');
 var banter =        require('./texts');
+var Redis =         require('ioredis');
 
 var app =  express();
 var server =        require('http').Server(app);
@@ -18,8 +19,6 @@ var port = process.env.PORT || 3002;
 let redisPort =     config.redis.port;
 let redisHost =     config.redis.host;
 let redisPassword = config.redis.password;
-
-var Redis =         require('ioredis');
 
 var redis = new Redis({
  port: redisPort,
@@ -32,15 +31,17 @@ var pub = new Redis({
 
 });
 
-// subscribe and listen
+// subscribe to a channel
 redis.subscribe('newMessage', function (err, count) {
 			console.log("Subscribed to " + count + " channel")
   });
-/*
+
+// log message when detected on redis channel
 redis.on('message', function (channel, message) {
     console.log('Received the following from channel ' + channel + ' message: ' + message);
   });
-*/
+
+// publish messages randomly -- test runner for chaltic platform
 function stream() {
   var msgObj = banter[Math.floor(Math.random() * banter.length)];
   var sendMsg = JSON.stringify(msgObj)
@@ -48,6 +49,7 @@ function stream() {
 
 }
 
+// control the rate of publishing messages
 function intervalObj() {
   setInterval(function() {
   console.log('publish');
@@ -55,8 +57,7 @@ function intervalObj() {
  }, 2000)};
 
 
-// keep server running
-
+// server spins up and initiates the publishing function
 server.listen(port, function() {
   console.log("Listening on port " + port)
   intervalObj()
