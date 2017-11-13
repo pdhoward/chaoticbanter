afbd1cf16fdcfd23e669517a317b6534546bb1c5
pdhoward <patrick.howard@hotmail.com>
2017-11-13 13:18:35 -0500

    updates

--- db/index.js
+++ db/index.js
@@ -27,7 +27,6 @@ Mongoose.Promise = global.Promise;
 module.exports = {
 	Mongoose,
 	models: {
-		user: require('./schemas/user.js'),
-		room: require('./schemas/room.js')
+		order: require('./schemas/orders.js')	
 	}
 };
